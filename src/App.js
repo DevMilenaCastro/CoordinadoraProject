de214301dfbe77f3396f8e0519aa8b5185549e7f
milenac <ingmilenacastro@hotmail.com>
2021-05-06 15:30:25 -0500

    Add dates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Bar from "./components/Bar";
 import UpTime from "./components/UpTime";
 import "./assets/styles/App.scss";
 
 function App() {
-  React.useEffect(() => {
-    // console.log("useEffect");
-    getData();
+  const [dates, setDates] = useState({});
+  useEffect(() => {
+    fetch("https://us-central1-cm-devops-294019.cloudfunctions.net/status")
+      .then((response) => response.json())
+      .then((data) => setDates(data));
   }, []);
-
-  const getData = async () => {
-    const data = await fetch(
-      "https://us-central1-cm-devops-294019.cloudfunctions.net/status"
-    );
-    const users = await data.json();
-    console.log(users);
-  };
 
   return (
     <div className="App">
       <Header />
-      <Bar />
+      <Bar dates={dates} />
       <UpTime />
     </div>
   );
--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import "../assets/styles/components/_bar.scss";
 
-const Bar = () => (
-  <section className="bar">
-    <div className="bar__blue">
-      <span className="bar__blue--circle"></span>
-      <p className="bar__blue--all">All systems operational</p>
-      <p className="bar__blue--refresh">Refreshed 12:11:00 UTC-0400</p>
-    </div>
-  </section>
-);
+const Bar = ({ dates }) => {
+  const prueba = (title) => (
+    <p className="bar__blue--refresh">Refreshed {title}</p>
+  );
+
+  const renderDates = () => {
+    if (dates?.last_updated) {
+      const date = new Date(dates.last_updated);
+      const formatedDate = `${date.getDate()} / ${date.getMonth()} / ${date.getFullYear()}`;
+      return prueba(formatedDate);
+    }
+    return null;
+  };
+
+  const renderTitles = () => {
+    if (dates?.status?.apis) {
+      return dates?.status?.apis.map((item) => {
+        console.log(item);
+        if (item.tracking) {
+          return prueba(item.tracking.title);
+        }
+        return prueba(item.authenticacion.title);
+      });
+    }
+    return null;
+  };
+
+  return (
+    <section className="bar">
+      <div className="bar__blue">
+        <span className="bar__blue--circle"></span>
+        <p className="bar__blue--all">All systems operational</p>
+        {renderDates()}
+      </div>
+    </section>
+  );
+};
 
 export default Bar;
